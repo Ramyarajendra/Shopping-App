d0c8b636aee804476b1c3ba0a1802e1ce9ce971f
RAMYA RAJENDRA <ramyarajendra@RAMYAs-MBP.cable.rcn.com>
2021-01-25 02:32:10 -0600

    Added Error Handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 
 dotenv.config()
@@ -12,6 +13,10 @@ connectDB()
 const app = express()
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
