a0da557d7aaaf326a6271522a457ef9df2d2ffb3
RAMYA RAJENDRA <ramyarajendra@RAMYAs-MBP.cable.rcn.com>
2021-01-31 22:26:56 -0600

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,17 @@ app.use('/api/upload', uploadRoutes )
 
 app.get('/api/config/paypal',(req,res)=> res.send(process.env.PAYPAL_CLIENT_ID))
 
-
 const __dirname = path.resolve() 
 // gives current directory 
 //__dirname is not available in ES modules so mimicing the same
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
 
 
 app.use(notFound)
